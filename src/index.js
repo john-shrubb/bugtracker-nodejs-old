d753c8536ba1d4c835e1f3163c11b6da5f283d31
John Shrubb <34843796+john-shrubb@users.noreply.github.com>
2023-04-04 16:41:05 +0100

    Add Auth0 package and different pages support

--- src/index.js
+++ src/index.js
@@ -5,14 +5,27 @@
 const Express = require('express');
 const app = Express();
 
+const fs = require('fs');
+
 const config = require('./config/webserver.json');
 
 // Simple hello world
 
 app.get('/', (req, res) => {
-    res.end('<h1>Hello</h1>');
+    res.setHeader('Content-Type', 'text/html');
+    const file = fs.readFileSync('./src/pages/dashboard.html');
+    res.statusCode = 200;
+    res.end(file.toString());
 });
 
 app.use('/assets', Express.static('src/static'));
 
-app.listen(config.prodport);
+app.use((req, res) => {
+    res.end('Error.');
+})
+
+app.listen(
+    config.prodport,
+    '0.0.0.0',
+    () => console.log('Server online on port ' + config.prodport)
+);
