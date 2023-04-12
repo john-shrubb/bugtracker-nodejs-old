f0d849efad99e2b4c28c07bde4de8ac8e039ba39
John Shrubb <34843796+john-shrubb@users.noreply.github.com>
2023-04-12 01:08:36 +0100

    Add deletion of ticket functionality to view ticket page and API.

--- src/api/comments.js
+++ src/api/comments.js
@@ -123,4 +123,28 @@ app.post('/api/comments/create/:ticketid', async (req, res) => {
 	});
 });
 
+app.get('/api/comments/delete/:id', async (req, res) => {
+	const userID = req.oidc.userID;
+	const commentID = req.params['id'];
+
+	const userRole = (await database.query('SELECT role FROM users WHERE user_id=$1;', [userID])).rows[0]['role'];
+
+	const commentOwner = (await database.query('SELECT * FROM comments WHERE user_id=$1 AND comment_id=$2;', [userID, commentID])).rows[0];
+
+	if (!commentOwner && userRole === 1) {
+		res.statusCode = 403;
+		res.json({
+			status: 403,
+			response: 'You are either not allowed to delete this comment or it does not exist.',
+		});
+		return;
+	}
+
+	await database.query('DELETE FROM comments WHERE comment_id=$1', [commentID]);
+	res.json({
+		status: 200,
+		response: 'Comment deleted.',
+	});
+});
+
 module.exports = app;
