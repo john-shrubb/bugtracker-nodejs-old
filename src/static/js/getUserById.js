ccc2d2097f02f60a8be9eec7881f9d72a639df58
John Shrubb <34843796+john-shrubb@users.noreply.github.com>
2023-04-21 11:05:44 +0100

    Optimisations to reduce calls to back end

--- src/static/js/getUserById.js
+++ src/static/js/getUserById.js
@@ -1,20 +1,30 @@
+// Create a user cache to prevent repetitive calls to the API
+
+const userCache = {};
+
 // Get user by ID
 		
 async function getUserById(userID) {
 	return await new Promise(function (resolve) {
-		const request = new XMLHttpRequest();
-		request.open('post', '/api/users/get/id');
-		request.setRequestHeader('Content-Type', 'application/json');
-		request.send(JSON.stringify({
-			userID: userID,
-		}));
-		request.onload = function() {
-			const response = JSON.parse(request.responseText);
-
-			if (response['status'] !== 200) {
-				throw new Error('Response from API: ' + response['response']);
-			}
-			resolve(response['response']);
-		};
+		if (!userCache[userID]) {
+			const request = new XMLHttpRequest();
+			request.open('post', '/api/users/get/id');
+			request.setRequestHeader('Content-Type', 'application/json');
+			request.send(JSON.stringify({
+				userID: userID,
+			}));
+			request.onload = function() {
+				const response = JSON.parse(request.responseText);
+	
+				if (response['status'] !== 200) {
+					throw new Error('Response from API: ' + response['response']);
+				}
+				userCache[userID] = response['response'];
+				console.log()
+				resolve(userCache[userID]);
+			};
+		} else {
+			resolve(userCache[userID]);
+		}
 	});
 }
